4d4ca17596d4349bc51be62d4acaa7564fbe7066
Skipper <155382339+Sklpp3r@users.noreply.github.com>
2025-03-28 13:08:11 +0300

    Update

--- Assets/Scripts/CharacterDataBase/CharacterDataBase.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "CharacterDataBase", menuName = "Scriptable Objects/CharacterDataBase")]
-public class CharacterDataBase : ScriptableObject
-{
-    public CharacterDataBase[] characters;
-    
-}
